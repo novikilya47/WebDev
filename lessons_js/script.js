b31e7daf6c6dfb81e46116494f41b4ba3962cb33
Ilya <novikilya-1998@mail.ru>
2021-04-23 01:38:05 +0300

    first commit

---
+++ lessons_js/script.js
@@ -0,0 +1,16 @@
+
+// Написание без плюсов
+let a=1+3;
+console.log(`мне ${a} лет`);
+
+
+// Проверка на число
+let old = Number("5g");
+console.log(old);
+if (isNaN(old) == true){
+    console.log("Это не число");
+} else{
+    console.log("Это число");
+}
+
+
